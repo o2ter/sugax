3bf1655e8a114cc2142a7ba8be237361a9fc19f2
Susan Cheng <susan.doggie@gmail.com>
2024-05-27 12:34:43 +0800

    Update index.ts

--- src/hooks/asyncResource/index.ts
+++ src/hooks/asyncResource/index.ts
@@ -39,41 +39,44 @@ export const useAsyncResource = <T>(
 
   const [state, setState] = React.useState<{
     count?: number;
-    updateFlag?: boolean;
+    flag?: boolean;
     resource?: T;
     error?: Error;
     token?: string;
     abort?: AbortController;
   }>({});
 
+  const _dispatch = (
+    token: string,
+    next: React.SetStateAction<typeof state>,
+  ) => setState(state => state.token === token ? ({
+    ...(_.isFunction(next) ? next(state.flag ? state : _.omit(state, 'resource', 'error')) : next),
+    count: state.flag ? state.count : (state.count ?? 0) + 1,
+    flag: true,
+  }) : state);
+
   const _refresh = useAsyncDebounce(async (abort: AbortController) => {
 
     const token = _.uniqueId();
-    setState(state => ({ ...state, token, abort, updateFlag: false }));
-
-    const _dispatch: typeof setState = (next) => setState(state => state.token === token ? ({
-      ...(_.isFunction(next) ? next(state.updateFlag ? state : _.omit(state, 'resource', 'error')) : next),
-      count: state.updateFlag ? state.count : (state.count ?? 0) + 1,
-      updateFlag: true,
-    }) : state);
+    setState(state => ({ ...state, token, abort, flag: false }));
 
     try {
 
       const resource = await fetch({
         abortSignal: abort.signal,
         dispatch: (next) => {
-          _dispatch(state => ({
+          _dispatch(token, state => ({
             ...state,
             resource: _.isFunction(next) ? next(state.resource) : next,
           }));
         },
       });
 
-      _dispatch(state => ({ resource: resource ?? state.resource }));
+      _dispatch(token, state => ({ resource: resource ?? state.resource }));
 
     } catch (error) {
 
-      _dispatch(state => ({
+      _dispatch(token, state => ({
         resource: state.resource,
         error: error as Error,
       }));
